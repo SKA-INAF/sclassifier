4a237950db21ec58d1f63fcc3f5b369efa82df1b
riggi <simone.riggi@gmail.com>
2021-09-20 18:10:28 +0200

    Modified module name

--- sclassifier_vae/__init__.py
+++ sclassifier_vae/__init__.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-__title__ = 'svaeclassifier'
+__title__ = 'sclassifier_vae'
 __version__ = '1.0.0'
 __author__ = 'Simone Riggi'
 __license__ = 'GPL3'
@@ -17,6 +17,6 @@ import logging.config
 logging.basicConfig(format="%(asctime)-15s %(levelname)s - %(message)s",datefmt='%Y-%m-%d %H:%M:%S')
 logger= logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-logger.info("This is svaeclassifier {0}-({1})".format(__version__, __date__))
+logger.info("This is sclassifier_vae {0}-({1})".format(__version__, __date__))
 
 
