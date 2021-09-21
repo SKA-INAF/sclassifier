9f40de4980b6e57b654e1ab17a43b7ae5ba522f4
riggi <simone.riggi@gmail.com>
2021-09-21 20:38:42 +0200

    Add generators in train, new model arc

--- sclassifier_vae/classifier.py
+++ sclassifier_vae/classifier.py
@@ -47,7 +47,8 @@ import matplotlib.pyplot as plt
 
 ## PACKAGE MODULES
 from .utils import Utils
-from .data_provider import DataProvider
+from .data_loader import DataLoader
+from .data_loader import SourceData
 
 ##############################
 ##     GLOBAL VARS
@@ -65,35 +66,52 @@ class VAEClassifier(object):
 			Arguments:
 				- encoder_nnarc_file: File with encoder network architecture to be created
 				- decoder_nnarc_file: File with decoder network architecture to be created
-				- DataProvider class
+				- DataLoader class
 	"""
 	
-	def __init__(self,data_provider):
+	def __init__(self, data_loader):
 		""" Return a Classifer object """
 
 		# - Input data
 		self.encoder_nnarc_file= ''
 		self.decoder_nnarc_file= ''
-		self.dp= data_provider
+		self.dl= data_loader
 
 		# - Train data	
 		self.nsamples_train= 0
-		self.nx= 0
-		self.ny= 0
-		self.nchannels= 1
+		self.nx= 128 
+		self.ny= 128
+		self.nchannels= 0
 		self.inputs= None	
 		self.inputs_train= None
+		self.input_labels= {}
+		self.source_names= []
 		self.flattened_inputs= None	
 		self.input_data_dim= 0
 		self.encoded_data= None
+		self.train_data_generator= None
+		self.test_data_generator= None
+		self.augmentation= False	
+		self.validation_steps= 10
+		self.use_multiprocessing= False
+		self.nworkers= 1
 		
 		# - NN architecture
 		self.use_shallow_network= True
 		self.fitout= None		
 		self.vae= None
 		self.encoder= None
-		self.decoder= None
+		self.decoder= None	
+		self.nfilters_cnn= [32,64,128]
+		self.kernsizes_cnn= [3,5,7]
+		self.strides_cnn= [2,2,2]
+		self.add_max_pooling= True
+		self.pool_size= 2
+		self.add_leakyrelu= False
+		self.leakyrelu_alpha= 0.2
+		self.add_batchnorm= True
 		self.nlayers_intermediate= 1
+		self.intermediate_layer_size_factor= 1
 		self.intermediate_dim= 512
 		self.intermediate_layer_activation= 'relu'
 		self.output_layer_activation= 'sigmoid'
@@ -108,6 +126,32 @@ class VAEClassifier(object):
 		self.learning_rate= 1.e-4
 		self.use_mse_loss= True
 
+		# - Draw options
+		self.marker_mapping= {
+			'UNKNOWN': 'o', # unknown
+			'MIXED_TYPE': 'X', # mixed type
+			'STAR': 'x', # star
+			'GALAXY': 'D', # galaxy
+			'PN': '+', # PN
+			'HII': 's', # HII
+			'YSO': 'P', # YSO
+			'QSO': 'v', # QSO
+			'PULSAR': 'd', # pulsar
+		}
+
+		self.marker_color_mapping= {
+			'UNKNOWN': 'k', # unknown
+			'MIXED_TYPE': 'tab:gray', # mixed type
+			'STAR': 'r', # star
+			'GALAXY': 'm', # galaxy
+			'PN': 'g', # PN
+			'HII': 'b', # HII
+			'YSO': 'y', # YSO
+			'QSO': 'c', # QSO
+			'PULSAR': 'tab:orange', # pulsar
+		}
+		
+
 		# - Output data
 		self.outfile_loss= 'nn_loss.png'
 		self.outfile_accuracy= 'nn_accuracy.png'
@@ -118,6 +162,11 @@ class VAEClassifier(object):
 	#####################################
 	##     SETTERS/GETTERS
 	#####################################
+	def set_image_size(self,nx,ny):
+		""" Set image size """	
+		self.nx= nx
+		self.ny= ny
+
 	def set_optimizer(self,opt):
 		""" Set optimizer """
 		self.optimizer= opt
@@ -138,30 +187,44 @@ class VAEClassifier(object):
 		""" Set intermediate layer size """
 		self.intermediate_dim= n
 
+	def set_n_intermediate_layers(self,n):
+		""" Set number of intermediate layers """
+		self.nlayers_intermediate= n
+
+	def set_intermediate_layer_size_factor(self,f):
+		""" Set reduction factor to compute number of neurons in dense layers """
+		self.intermediate_layer_size_factor= f
+
 	#####################################
 	##     SET TRAIN DATA
 	#####################################
 	def __set_data(self):
-		""" Set train data from provider """
+		""" Set train data & generator from loader """
 
-		# - Retrieve input data info from provider
-		self.inputs_train= self.dp.get_data()
-		imgshape= self.inputs_train.shape
-			
-		# - Check if data provider has data filled
-		if self.inputs_train.ndim!=4:
-			logger.error("Invalid number of dimensions in train data (4 expected) (hint: check if data was read in provider!)")
-			return -1
+		# - Retrieve info from data loader
+		self.nchannels= self.dl.nchannels
+		self.source_labels= self.dl.labels
+		self.source_ids= self.dl.class_ids
+		self.source_names= self.dl.snames
+		self.nsamples_train= len(self.source_labels)
 
-		# - Set data
-		self.nsamples_train= imgshape[0]
-		self.nx= imgshape[2]
-		self.ny= imgshape[1]
-		self.nchannels= imgshape[3] 
+		# - Create train data generator
+		self.train_data_generator= self.dp.data_generator(
+			batch_size=self.batch_size, 
+			shuffle=True,
+			resize=True, nx=self.nx, ny=self.ny, 
+			normalize=True, 
+			augment=self.augmentation
+		)	
+
+		self.test_data_generator= self.dp.data_generator(
+			batch_size=self.nsamples_train, 
+			shuffle=False,
+			resize=True, nx=self.nx, ny=self.ny, 
+			normalize=True, 
+			augment=False
+		)	
 		
-		logger.info("Train data size (N,nx,ny,nchan)=(%d,%d,%d,%d)" % (self.nsamples_train,self.nx,self.ny,self.nchannels))
-		
-
 		return 0
 
 	#####################################
@@ -216,7 +279,8 @@ class VAEClassifier(object):
 		# - Build model
 		logger.info("Creating VAE model ...")
 		self.flattened_outputs = self.decoder(self.encoder(self.inputs)[2])
-		self.outputs= layers.Reshape( (self.nx,self.ny,self.nchannels) )(self.flattened_outputs)
+		#self.outputs= layers.Reshape( (self.nx,self.ny,self.nchannels) )(self.flattened_outputs)
+		self.outputs= layers.Reshape( (self.ny,self.nx,self.nchannels) )(self.flattened_outputs)
 		self.vae = Model(self.inputs, self.outputs, name='vae_mlp')
 
 		# - Set model loss = mse_loss or xent_loss + kl_loss
@@ -264,8 +328,11 @@ class VAEClassifier(object):
 		print("Input data dim=", self.input_data_dim)
 
 		# - Intermediate layers
+		layer_size= self.intermediate_dim
 		for index in range(self.nlayers_intermediate):
-			x = layers.Dense(self.intermediate_dim, activation=self.intermediate_layer_activation)(x)
+			if index>0:
+				layer_size/= self.intermediate_layer_size_factor
+			x = layers.Dense(int(layer_size), activation=self.intermediate_layer_activation)(x)
 
 		# - Output layers
 		self.z_mean = layers.Dense(self.latent_dim,name='z_mean')(x)
@@ -293,8 +360,15 @@ class VAEClassifier(object):
 		x= latent_inputs
 
 		# - Intermediate layers
+		layer_size= self.intermediate_dim
 		for index in range(self.nlayers_intermediate):
-			x = layers.Dense(self.intermediate_dim, activation=self.intermediate_layer_activation)(x)
+			if index>0:
+				layer_size/= self.intermediate_layer_size_factor
+
+		for index in range(self.nlayers_intermediate):	
+			if index>0:
+				layer_size*= self.intermediate_layer_size_factor
+			x = layers.Dense(int(layer_size), activation=self.intermediate_layer_activation)(x)
 
 		# - Output layer
 		print("self.input_data_dim type=",type(self.input_data_dim))
@@ -309,6 +383,164 @@ class VAEClassifier(object):
 		plot_model(self.decoder, to_file='vae_mlp_decoder.png', show_shapes=True)
 
 		return 0
+
+
+
+	#####################################
+	##     BUILD PARAMETRIZED NETWORK
+	#####################################
+	def __build_parametrized_network(self):
+		""" Build VAE model parametrized architecture """
+	
+		#===========================
+		#==   CREATE ENCODER
+		#===========================	
+		logger.info("Creating parametrized encoder network ...")
+		status= self.__build_parametrized_encoder()
+		if status<0:
+			logger.error("Encoder model creation failed!")
+			return -1
+		
+		#===========================
+		#==   CREATE DECODER
+		#===========================	
+		logger.info("Creating parametrized decoder network ...")
+		status= self.__build_parametrized_decoder()
+		if status<0:
+			logger.error("Decoder model creation failed!")
+			return -1
+
+		#===========================
+		#==   CREATE VAE MODEL
+		#===========================	
+		# - Build model
+		logger.info("Creating VAE model ...")
+		self.flattened_outputs = self.decoder(self.encoder(self.inputs)[2])
+		#self.outputs= layers.Reshape( (self.nx,self.ny,self.nchannels) )(self.flattened_outputs)
+		self.outputs= layers.Reshape( (self.ny,self.nx,self.nchannels) )(self.flattened_outputs)
+		self.vae = Model(self.inputs, self.outputs, name='vae_mlp')
+
+		# - Set model loss = mse_loss or xent_loss + kl_loss
+		# Reconstruction loss
+		if self.use_mse_loss:
+			reconstruction_loss = mse(self.flattened_inputs,self.flattened_outputs)
+		else:
+			reconstruction_loss = binary_crossentropy(self.flattened_inputs,self.flattened_outputs)
+
+		reconstruction_loss*= self.input_data_dim[1]
+
+		# Kl loss
+		kl_loss = 1 + self.z_log_var - K.square(self.z_mean) - K.exp(self.z_log_var)
+		kl_loss = K.sum(kl_loss, axis=-1)
+		kl_loss*= -0.5
+
+		# Total loss
+		vae_loss = K.mean(reconstruction_loss + kl_loss)
+
+		self.vae.add_loss(vae_loss)
+		self.vae.compile(optimizer=self.optimizer)
+    
+		# - Print and draw model
+		self.vae.summary()
+		plot_model(self.vae,to_file='vae_mlp.png',show_shapes=True)
+
+		return 0
+
+	def __build_parametrized_encoder(self):
+		""" Build encoder parametrized network """
+		
+		# - Input layer	
+		inputShape = (self.ny, self.nx, self.nchannels)
+		self.inputs= Input(shape=inputShape,dtype='float', name='encoder_input')
+		x= self.inputs
+
+		# - Create a number of CNN layers
+		for k in range(len(self.nfilters_cnn)):
+
+			# - Add a Convolutional 2D layer
+			padding= "same"
+			x = layers.Conv2D(self.nfilters_cnn[k], (self.kernsizes_cnn[k], self.kernsizes_cnn[k]), strides=self.strides_cnn[k], padding=padding)(x)
+
+			# - Add max pooling?
+			if self.add_max_pooling:
+				padding= "valid"
+				x = layers.MaxPooling2D(pool_size=(self.pool_size,self.pool_size),strides=None,padding=padding)(x)
+
+			# - Add Leaky RELU?	
+			if self.add_leakyrelu:
+				x = layers.LeakyReLU(alpha=self.leakyrelu_alpha)(x)
+
+			# - Add batch normalization?
+			if self.add_batchnorm:
+				x = BatchNormalization(axis=-1)(x)
+		
+		# - Store layer size before flattening (needed for decoder network)
+		self.shape_before_flattening= K.int_shape(x)
+
+		# - Flatten layer
+		x = layers.Flatten()(x)
+		self.flattened_inputs= x
+		self.input_data_dim= K.int_shape(x)
+		print("Input data dim=", self.input_data_dim)
+
+		# - Output layers
+		self.z_mean = layers.Dense(self.latent_dim,name='z_mean')(x)
+		self.z_log_var = layers.Dense(self.latent_dim,name='z_log_var')(x)
+		self.z = Lambda(self.__sampling, output_shape=(self.latent_dim,), name='z')([self.z_mean, self.z_log_var])
+
+		# - Instantiate encoder model
+		self.encoder = Model(self.inputs, [self.z_mean, self.z_log_var, self.z], name='encoder')
+
+		# - Print and plot model
+		self.encoder.summary()
+		plot_model(self.encoder, to_file='vae_mlp_encoder.png', show_shapes=True)
+
+		return 0
+
+
+	def __build_parametrized_decoder(self):
+		""" Build decoder parametrized network """
+
+		# - Input layer (equal to encoder output)
+		latent_inputs = Input(shape=(self.latent_dim,), dtype='float', name='z_sampling')
+		x= latent_inputs
+
+		# - Add dense layer and reshape
+		x = layers.Dense(np.prod(self.shape_before_flattening[1:]))(x)
+		x = layers.Reshape((self.shape_before_flattening[1], self.shape_before_flattening[2], self.shape_before_flattening[3]))(x)
+		
+		# - Create a number of CNN layers in reverse order
+		for k in reversed(range(len(self.nfilters_cnn))):
+			# - Add deconv 2D layer
+			padding= "same"
+			x = layers.Conv2DTranspose(self.nfilters_cnn[k], (self.kernsizes_cnn[k], self.kernsizes_cnn[k]), strides=self.strides_cnn[k], padding=padding)(x)
+
+			# - Add max pooling?
+			if self.add_max_pooling:
+				padding= "valid"
+				x = layers.MaxPooling2D(pool_size=(self.pool_size,self.pool_size),strides=None,padding=padding)(x)
+
+			# - Add Leaky RELU?	
+			if self.add_leakyrelu:
+				x = layers.LeakyReLU(alpha=self.leakyrelu_alpha)(x)
+
+			# - Add batch normalization?
+			if self.add_batchnorm:
+				x = BatchNormalization(axis=-1)(x)
+
+
+		# - Apply a single conv (or Conv tranpose??) layer to recover the original depth of the image
+		padding= "same"
+		x = layers.Conv2D(self.nchannels, (3, 3), activation='sigmoid', padding=padding)(x)
+		#x = layers.Conv2DTranspose(self.nchannels, (3, 3), activation='sigmoid', padding=padding)(x)
+		outputs = x
+
+		# - Create decoder model
+		self.decoder = Model(latent_inputs, outputs, name='decoder')
+
+		# - Print and draw model		
+		self.decoder.summary()
+		plot_model(self.decoder, to_file='vae_mlp_decoder.png', show_shapes=True)
 
 
 	#####################################
@@ -473,17 +705,31 @@ class VAEClassifier(object):
 		self.train_loss_vs_epoch= np.zeros((1,self.nepochs))	
 		deltaLoss_train= 0
 
+		steps_per_epoch= self.nsamples_train // self.batch_size
+
 		#===========================
 		#==   TRAIN VAE
 		#===========================
-		logger.info("Start VAE training ...")
+		logger.info("Start VAE training (dataset_size=%d, batch_size=%d, steps_per_epoch=%d) ..." % (self.nsamples_train, self.batch_size, steps_per_epoch))
 		for epoch in range(self.nepochs):
+	
+			#self.fitout= self.vae.fit(
+			#	x=self.inputs_train,
+			#	epochs=1,
+			#	batch_size=self.batch_size,
+			#	validation_data=(self.inputs_train, None),
+			#	verbose=1
+			#)
+
 			self.fitout= self.vae.fit(
-				x=self.inputs_train,
+				x=self.train_data_generator,
 				epochs=1,
-				batch_size=self.batch_size,
-				validation_data=(self.inputs_train, None),
-				verbose=1
+				steps_per_epoch=steps_per_epoch,
+				validation_data=self.train_data_generator,
+				validation_steps=self.validation_steps,
+				use_multiprocessing=self.use_multiprocessing,
+				workers=self.nworkers,
+				verbose=2
 			)
 
 			loss_train= self.fitout.history['loss'][0]
@@ -537,14 +783,35 @@ class VAEClassifier(object):
 		#==   SAVE ENCODED DATA
 		#================================
 		logger.info("Saving encoded data to file ...")
-		self.encoded_data, _, _= self.encoder.predict(self.inputs_train, batch_size=self.batch_size)
-		print("encoded_data type=",type(self.encoded_data))
-		print("encoded_data len=",len(self.encoded_data))
-		print("encoded_data=",self.encoded_data)
-		N= self.encoded_data.shape[1]
+		#self.encoded_data, _, _= self.encoder.predict(self.inputs_train, batch_size=self.batch_size)
+		self.encoded_data, _, _= self.encoder.predict(
+			x=self.test_data_generator,
+    	verbose=2,
+    	workers=self.nworkers,
+    	use_multiprocessing=self.use_multiprocessing
+		)
+
+		#print("encoded_data type=",type(self.encoded_data))
+		#print("encoded_data len=",len(self.encoded_data))
+		#print("encoded_data=",self.encoded_data)
+		print("encoded_data shape")
+		print(self.encoded_data.shape)
+		N= self.encoded_data.shape[0]
+		Nvar= self.encoded_data.shape[1]
 		
-		head= '# z1 - z2'
-		Utils.write_ascii(self.encoded_data,self.outfile_encoded_data,head)	
+		# - Merge encoded data
+		obj_names= np.array(self.source_names).reshape(N,1)
+		obj_ids= np.array(self.source_ids).reshape(N,1)
+		enc_data= np.concatenate(
+			(obj_names, self.encoded_data, obj_ids),
+			axis=1
+		)
+
+		znames_counter= list(range(1,Nvar+1))
+		znames= '{}{}'.format('z',' z'.join(str(item) for item in znames_counter))
+		head= '{} {} {}'.format("# sname",znames,"id")
+		#Utils.write_ascii(self.encoded_data,self.outfile_encoded_data,head)	
+		Utils.write_ascii(enc_data,self.outfile_encoded_data,head)	
 
 
 		return 0
@@ -559,7 +826,7 @@ class VAEClassifier(object):
 		#===========================
 		#==   SET TRAINING DATA
 		#===========================	
-		logger.info("Setting training data from provider ...")
+		logger.info("Setting training data from data loader ...")
 		status= self.__set_data()
 		if status<0:
 			logger.error("Train data set failed!")
@@ -572,8 +839,9 @@ class VAEClassifier(object):
 		logger.info("Building network architecture ...")
 		if self.use_shallow_network:
 			status= self.__build_shallow_network()
-		#else:
-		#	status= self.__build_network(self.nnarc_file)
+		else:
+			status= self.__build_parametrized_network()
+			#status= self.__build_network(self.nnarc_file)
 
 		if status<0:
 			logger.error("NN build failed!")
@@ -623,7 +891,23 @@ class VAEClassifier(object):
 		# - Display a 2D plot of the encoded data in the latent space
 		logger.info("Plot a 2D plot of the encoded data in the latent space ...")
 		plt.figure(figsize=(12, 10))
-		plt.scatter(self.encoded_data[:, 0], self.encoded_data[:, 1])
+
+		N= self.encoded_data.shape[0]
+		print("N=%d" % N)
+		for i in range(N):
+			source_name= self.source_names[i]
+			source_label= self.source_labels[i]
+			marker= 'o'
+			color= 'k'
+			obj_id= 0
+			has_label= source_label in self.marker_mapping
+			if has_label:
+				marker= self.marker_mapping[source_label]
+				color= self.marker_color_mapping[source_label]
+
+			plt.scatter(self.encoded_data[i,0], self.encoded_data[i,1], color=color, marker=marker)
+
+		#plt.scatter(self.encoded_data[:, 0], self.encoded_data[:, 1])
 		#plt.colorbar()
 		plt.xlabel("z0")
 		plt.ylabel("z1")
---
+++ sclassifier_vae/data_loader.py
@@ -0,0 +1,385 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+##################################################
+###          MODULE IMPORT
+##################################################
+## STANDARD MODULES
+import os
+import sys
+import subprocess
+import string
+import time
+import signal
+from threading import Thread
+import datetime
+import numpy as np
+import random
+import math
+import logging
+from collections import Counter
+import json
+
+## ASTROPY MODULES 
+from astropy.io import ascii 
+
+## ADDON ML MODULES
+from sklearn.model_selection import train_test_split
+from imgaug import augmenters as iaa
+
+## PACKAGE MODULES
+from .utils import Utils
+
+##############################
+##     GLOBAL VARS
+##############################
+from sclassifier import logger
+
+
+##############################
+##     SOURCE DATA CLASS
+##############################
+class SourceData(object):
+	""" Source data class """
+
+	def __init__(self):
+		""" Create a source data object """
+	
+		# - Init vars
+		self.filepaths= []
+		self.sname= "XXX"
+		self.label= "UNKNOWN"
+		self.id= -1
+		self.f_badpix_thr= 0.3
+		self.img_data= []
+		self.img_heads= []
+		self.img_cube= None
+		self.nx= 0
+		self.ny= 0
+		self.nchannels= 0
+
+		
+	def set_from_dict(self, d):
+		""" Set source data from input dictionary """ 
+		try:
+			self.filepaths= d["filepaths"]
+			self.sname= d["sname"]
+			self.label= d["label"]
+			self.id= d["id"]
+		except:
+			logger.warn("Failed to read values from given dictionary, check keys!")
+			return -1
+
+		return 0
+
+	def read_imgs(self):
+		""" Read image data from paths """
+
+		# - Check data filelists
+		if not self.filepaths:
+			logger.error("Empty filelists given!")
+			return -1
+
+		# - Read images
+		nimgs= len(self.filepaths)
+		self.nchannels= nimgs
+
+		for filename in self.filepaths:
+			# - Read image
+			logger.info("Reading file %s ..." % filename) 
+			data= None
+			try:
+				data, header= Utils.read_fits(filename)
+			except Exception as e:
+				logger.error("Failed to read image data from file %s (err=%s)!" % (filename,str(e)))
+				return -1
+		
+			# - Check image integrity
+			npixels= data.size
+			npixels_nan= np.count_nonzero(np.isnan(data)) 
+			npixels_inf= np.count_nonzero(np.isinf(data))
+			f_badpix= (npixels_nan + npixels_inf)/float(npixels)
+			if f_badpix>self.f_badpix_thr:
+				logger.warn("Image %s has too many bad pixels (f=%f>%f)!" % (filename,f_badpix,self.f_badpix_thr) )	
+				return -1
+
+			# - Append image channel data to list
+			self.img_data.append(data)
+			self.img_heads.append(header)
+		
+		# - Check image sizes
+		if not self.check_img_sizes():
+			logger.error("Image channels for source %s do not have the same size, check your dataset!" % self.sname)
+			return -1
+
+		# - Set data cube
+		self.img_cube= np.dstack(self.img_data)
+		self.nx= self.img_cube.shape[1]
+		self.ny= self.img_cube.shape[0]
+		self.nchannels= self.img_cube.shape[-1]
+
+		return 0
+
+	def check_img_sizes(self):
+		""" Check if images have the same size """
+		
+		# - Return false if no images are stored
+		if not self.img_data:
+			return False
+
+		# - Compare image sizes across different channels
+		same_size= True
+		nx_tmp= 0
+		ny_tmp= 0
+		for i in range(len(self.img_data)):
+			imgsize= np.shape(self.img_data)
+			nx= imgsize[1]
+			ny= imgsize[0]
+			if i==0:
+				nx_tmp= nx
+				ny_tmp= ny	
+			else:
+				if (nx!=nx_tmp or ny!=ny_tmp):
+					logger.debug("Image %s has different size (%d,%d) wrt to previous images (%d,%d)!" % (self.filepaths[i],nx,ny,nx_tmp,ny_tmp))
+					same_size= False
+
+		return same_size
+
+	
+	def resize_imgs(self, nx, ny, preserve_range=True):
+		""" Resize images to the same size """
+
+		# - Return if data cube is None
+		if self.img_cube is None:
+			logger.error("Image data cube is None!")
+			return -1
+
+		# - Check if resizing is needed
+		size_changed= (nx==self.nx) and (ny==self.ny)
+		if not size_changed:
+			logger.debug("Images have already the desired size (%d,%d), nothing to be done..." % (nx,ny))
+			return 0
+
+		# - Resize data cube
+		try:
+			data_resized= Utils.resize_img(self.img_cube, (ny, nx, ), preserve_range=True)
+		except Exception as e:
+			logger.warn("Failed to resize data to size (%")
+			return -1
+		
+		self.img_cube= data_resized
+
+		# - Update image sizes
+		self.nx= self.img_cube.shape[1]
+		self.ny= self.img_cube.shape[0]
+		self.nchannels= self.img_cube.shape[-1]
+
+		return 0	
+		
+
+	def normalize_imgs(self):
+		""" Normalize images in range [0,1] """
+
+		# - Return if data cube is None
+		if self.img_cube is None:
+			logger.error("Image data cube is None!")
+			return -1
+
+		# - Find min & max across all channels
+		#   NB: Excluding masked pixels (=0)
+		data_masked= np.ma.masked_equal(self.img_cube, 0.0, copy=False)
+		data_min= data_masked.min()
+		data_max= data_masked.max()
+
+		# - Normalize in range [0,1]
+		data_norm= (self.img_cube-data_min)/(data_max-data_min)
+		data_norm[self.img_cube==0]= 0
+		self.img_cube= data_norm
+	
+		return 0
+
+	def augment_imgs(self, augmenter):
+		""" Augment images """
+
+		# - Return if data cube is None
+		if self.img_cube is None:
+			logger.error("Image data cube is None!")
+			return -1
+
+		# - Augment data cube
+		try:
+			data_aug= augmenter(images=self.img_cube)
+		except Exception as e:
+			logger.error("Failed to augment data (err=%s)!" % str(e))
+			return -1
+
+		self.img_cube= data_aug
+
+		return 0
+
+##############################
+##     DATA LOADER
+##############################
+class DataLoader(object):
+
+	""" Read data from disk and provide it to the network
+
+			Arguments:
+				- datalist: Filelist (json) with input data
+				
+	"""
+	
+	def __init__(self, filename):
+		""" Return a DataLoader object """
+
+		# - Input data
+		self.datalistfile= filename
+		self.datalist= {}
+		self.datasize= 0
+		self.classids= []
+		self.classfract_map= {}
+		self.labels= []
+		self.snames= []
+		self.nchannels= 0
+
+		# - Define augmenters
+		naugmenters_applied= 2
+		self.augmenter= iaa.SomeOf((0,naugmenters_applied),
+			[
+  			iaa.Fliplr(1.0),
+    		iaa.Flipud(1.0),
+    		iaa.Affine(rotate=(-90, 90), mode='constant', cval=0.0),
+				#iaa.Affine(scale=(0.5, 1.5), mode='constant', cval=0.0),
+				iaa.Affine(translate_percent={"x": (-0.1, 0.1), "y": (-0.1, 0.1)}, mode='constant', cval=0.0)
+			],
+			random_order=True
+		)
+		
+	
+	#############################
+	##     READ INPUT DATA
+	#############################
+	def read_datalist(self):
+		""" Read json filelist """
+
+		# - Read data list
+		self.datalist= {}
+		try:
+			with open(self.datalistfile) as fp:
+				self.datalist= json.load(fp)
+		except Exception as e:
+			logger.error("Failed to read data filelist %s!" % self.datalistfile)
+			return -1
+
+		# - Check number of channels per image
+		nchannels_set= set([len(item["filepaths"]) for item in self.datalist["data"]])
+		if len(nchannels_set)!=1:
+			logger.warn("Number of channels in each object instance is different!")
+			return -1
+		
+		self.nchannels= list(nchannels_set)[0]
+
+		# - Inspect data (store number of instances per class, etc)
+		self.datasize= len(self.datalist["data"])
+		self.labels= [item["label"] for item in self.datalist["data"]]
+		self.snames= [item["sname"] for item in self.datalist["data"]]
+		self.classids= 	[item["id"] for item in self.datalist["data"]]
+		self.classfract_map= dict(Counter(classids).items())
+
+		logger.info("#%d objects in dataset" % self.datasize)
+
+		return 0
+
+	def read_data(self, index, resize=True, nx=128, ny=128, normalize=True, augment=False):	
+		""" Read data at given index """
+
+		# - Check index
+		if index<0 or index>=self.datasize:
+			logger.error("Invalid index %d given!" % index)
+			return None
+
+		# - Read source image data
+		logger.debug("Reading source image data %d ..." % index)
+		d= self.datasize[index]
+		sdata= SourceData()
+		if sdata.set_from_dict()<0:
+			logger.error("Failed to set source image data %d!" % index)
+			return None
+
+		if sdata.read_imgs()<0:
+			logger.error("Failed to read source images %d!" % index)
+			return None
+
+		# - Run augmentation?
+		if augment:
+			if sdata.augment_imgs(self.augmenter)<0:
+				logger.error("Failed to augment source image %d!" % index)
+				return None
+
+		# - Resize image?
+		if resize:
+			if sdata.resize_imgs(nx, ny, preserve_range=True)<0:
+				logger.error("Failed to resize source image %d to size (%d,%d)!" % (index,nx,ny))
+				return None
+			
+		# - Normalize image?
+		if normalize:
+			if sdata.normalize_imgs()<0:
+				logger.error("Failed to normalize source image %d!" % index)
+				return None
+
+		return sdata
+
+	###################################
+	##     GENERATE DATA FOR TRAINING
+	###################################
+	def data_generator(self, batch_size=1, shuffle=True, resize=True, nx=128, ny=128, normalize=True, augment=False):	
+		""" Generator function reading nsamples images from disk and returning to caller """
+	
+		nb= 0
+		data_index= -1
+		data_indexes= np.arange(0,self.datasize)
+
+		while True:
+			try:
+
+				# - Generate random data index and read data at this index
+				data_index = (data_index + 1) % self.datasize
+				if shuffle:
+					data_index= np.random.choice(data_indexes)
+				
+				sdata= self.read_data(
+					data_index, 
+					resize=resize, nx=nx, ny=ny
+					normalize=normalize, 
+					augment=augment
+				)
+				if sdata is None:
+					logger.warn("Failed to read source data at index %d, skip to next ..." % data_index)
+					continue
+
+				data_shape= sdata.img_cube.shape
+				inputs_shape= (nb,)+ data_shape
+
+				# - Initialize return data
+				if nb==0:
+					inputs= np.zeros(inputs_shape, dtype=np.float32)
+				
+				# - Update inputs
+				inputs[nb]= sdata.img_cube
+				nb+= 1
+
+				# - Return data if number of batch is reached and restart the batch
+				if nb>=batch_size:
+					yield (inputs,inputs)
+					nb= 0
+
+			except (GeneratorExit, KeyboardInterrupt):
+				logger.warn("Generator or keyboard exception catched while generating data...")
+				raise
+			except Exception as e:
+				logger.warn("Exception catched while generating data (err=%d) ..." % str(e))
+				raise
+			
+
