1976da477cb20103cdabfe35b8118aac46f15ec4
riggi <simone.riggi@gmail.com>
2021-09-24 16:32:09 +0200

    Add imgaug dep

--- sclassifier_vae/classifier.py
+++ sclassifier_vae/classifier.py
@@ -590,7 +590,11 @@ class VAEClassifier(object):
 		# - Check if vectors are not empty
 		y_true_isempty= tf.equal(tf.size(y_true_flattened_masked),0)	
 		y_pred_isempty= tf.equal(tf.size(y_pred_flattened_masked),0)
-		are_empty= tf.logical_or(y_true_isempty,y_pred_isempty).numpy()
+		are_empty_tensor= tf.logical_or(y_true_isempty,y_pred_isempty)
+		if tf.eagerly():
+			are_empty= are_empty_tensor.numpy()
+		else:
+			are_empty= are_empty_tensor.eval()
 
 		# - Compute reconstruction loss term
 		#logger.info("Computing the reconstruction loss ...")
