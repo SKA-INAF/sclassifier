966c0e22fb6bd88b7de17879f6ba540ba3a33a9f
riggi <simone.riggi@gmail.com>
2021-09-25 15:27:30 +0200

    Fix syntax error

--- scripts/run_train.py
+++ scripts/run_train.py
@@ -29,6 +29,7 @@ from sclassifier_vae import __version__, __date__
 from sclassifier_vae import logger
 from sclassifier_vae.data_loader import DataLoader
 from sclassifier_vae.classifier import VAEClassifier
+from sclassifier_vae.classifier_umap import UMAPClassifier
 
 #### GET SCRIPT ARGS ####
 def str2bool(v):
