67ee9186787ef4c952c8097639ffdf1e1a944094
riggi <simone.riggi@gmail.com>
2021-11-03 01:04:54 +0100

    Add script to run clustering on latent data

---
+++ scripts/run_clustering.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+##################################################
+###    SET SEED FOR REPRODUCIBILITY (DEBUG)
+##################################################
+#from numpy.random import seed
+#seed(1)
+#import tensorflow
+#tensorflow.random.set_seed(2)
+
+##################################################
+###          MODULE IMPORT
+##################################################
+## STANDARD MODULES
+import os
+import sys
+import subprocess
+import string
+import time
+import signal
+from threading import Thread
+import datetime
+import numpy as np
+import random
+import math
+import logging
+
+## COMMAND-LINE ARG MODULES
+import getopt
+import argparse
+import collections
+
+## MODULES
+from sclassifier_vae import __version__, __date__
+from sclassifier_vae import logger
+from sclassifier_vae.data_loader import DataLoader
+from sclassifier_vae.classifier import VAEClassifier
+from sclassifier_vae.classifier_umap import UMAPClassifier
+from sclassifier_vae.clustering import Clusterer
+
+#### GET SCRIPT ARGS ####
+def str2bool(v):
+	if v.lower() in ('yes', 'true', 't', 'y', '1'):
+		return True
+	elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+		return False
+	else:
+		raise argparse.ArgumentTypeError('Boolean value expected.')
+
+###########################
+##     ARGS
+###########################
+def get_args():
+	"""This function parses and return arguments passed in"""
+	parser = argparse.ArgumentParser(description="Parse args.")
+
+	# - Input options
+	parser.add_argument('-inputfile','--inputfile', dest='inputfile', required=True, type=str, help='Input feature data table filename') 
+	
+	# - Clustering options
+	parser.add_argument('-min_cluster_size', '--min_cluster_size', dest='min_cluster_size', required=False, type=int, default=5, action='store',help='Minimum cluster size for HDBSCAN clustering (default=5)')
+	parser.add_argument('-min_samples', '--min_samples', dest='min_samples', required=False, type=int, default=None, action='store',help='Minimum cluster sample parameter for HDBSCAN clustering. Typically equal to min_cluster_size (default=None')	
+	parser.add_argument('-modelfile_clust', '--modelfile_clust', dest='modelfile_clust', required=True, type=str, action='store',help='Clustering model filename (.h5)')
+	parser.add_argument('--predict_clust', dest='predict_clust', action='store_true',help='Only predict clustering according to current clustering model (default=false)')	
+	parser.set_defaults(predict_clust=False)
+
+	args = parser.parse_args()	
+
+	return args
+
+
+
+##############
+##   MAIN   ##
+##############
+def main():
+	"""Main function"""
+
+	
+	#===========================
+	#==   PARSE ARGS
+	#===========================
+	logger.info("Get script args ...")
+	try:
+		args= get_args()
+	except Exception as ex:
+		logger.error("Failed to get and parse options (err=%s)",str(ex))
+		return 1
+
+	# - Input filelist
+	inputfile= args.inputfile
+
+	# - Clustering options
+	min_cluster_size= args.min_cluster_size
+	min_samples= args.min_samples	
+	modelfile_clust= args.modelfile_clust
+	predict_clust= args.predict_clust
+
+	#===========================
+	#==   READ FEATURE DATA
+	#===========================
+	ret= Utils.read_feature_data(inputfile)
+	if not ret:
+		logger.error("Failed to read data from file %s!" % (inputfile))
+		return 1
+
+	data= ret[0]
+	snames= ret[1]
+	classids= ret[2]
+
+	#==============================
+	#==   RUN CLUSTERING
+	#==============================
+	logger.info("Running HDBSCAN classifier prediction on input feature data ...")
+	clust_class= Clusterer()
+	clust_class.min_cluster_size= min_cluster_size
+	clust_class.min_samples= min_samples
+	
+	status= 0
+	if predict_clust:
+		if clust_class.run_predict(vae_data, class_ids=classids, snames=snames, modelfile=modelfile_clust)<0:
+			logger.error("Clustering predict failed!")
+			return 1
+	else:
+		if clust_class.run_clustering(vae_data, class_ids=classids, snames=snames, modelfile=modelfile_clust)<0:
+			logger.error("Clustering run failed!")
+			return 1
+
+	return 0
+
+###################
+##   MAIN EXEC   ##
+###################
+if __name__ == "__main__":
+	sys.exit(main())
+
