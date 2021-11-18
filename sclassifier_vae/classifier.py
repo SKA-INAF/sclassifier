1e392bac32ed5bfa79918fad3a0f6e9509aff833
riggi <simone.riggi@gmail.com>
2021-11-18 20:26:37 +0100

    Modified loss functions, add cmd line options

--- sclassifier_vae/classifier.py
+++ sclassifier_vae/classifier.py
@@ -114,18 +114,18 @@ def ssim_batchavg(img1, img2, max_val, filter_size=11, filter_sigma=1.5, k1=0.01
 		# Compute ssim for all batch and store in list		
 		data_shape= tf.shape(img1)
 		tf.print("data_shape:", data_shape, output_stream=sys.stdout)
-		nsamples= data_shape[0]
+		nsamples= int(data_shape[0])
 		tf.print("type(nsamples):", type(nsamples), output_stream=sys.stdout)
 		tf.print("nsamples:", nsamples, output_stream=sys.stdout)
 		ssim_list= []
-		#for i in range(nsamples):
-		#	ssim_curr= ssim_chanavg(img1[i,:,:,:], img2[i,:,:,:], max_val, filter_size, filter_sigma, k1, k2)
-		#	ssim_list.append(ssim_curr)
+		for i in range(nsamples):
+			ssim_curr= ssim_chanavg(img1[i,:,:,:], img2[i,:,:,:], max_val, filter_size, filter_sigma, k1, k2)
+			ssim_list.append(ssim_curr)
 
 		# Compute mean over batch size
-		#ssim_tensor= tf.stack(ssim_list)
-		#ssim_batch_mean= tf.reduce_mean(ssim_tensor)
-		ssim_batch_mean= 1
+		ssim_tensor= tf.stack(ssim_list)
+		ssim_batch_mean= tf.reduce_mean(ssim_tensor)
+		##ssim_batch_mean= 1
 
 		return ssim_batch_mean
 
