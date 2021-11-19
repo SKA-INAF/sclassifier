ae0cb466c515eb44bf7c8e60ba01e5d707cd38cc
riggi <simone.riggi@gmail.com>
2021-11-19 11:00:41 +0100

    Set multiprocessing to false

--- sclassifier_vae/classifier.py
+++ sclassifier_vae/classifier.py
@@ -261,7 +261,7 @@ class VAEClassifier(object):
 		self.data_generator= None
 		self.augmentation= False	
 		self.validation_steps= 10
-		self.use_multiprocessing= True
+		self.use_multiprocessing= False
 		self.nworkers= 1
 		
 		# - NN architecture
