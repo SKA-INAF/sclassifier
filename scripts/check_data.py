fa4ac32d4c2c544d0e23b50b09c77596583a713c
riggi <simone.riggi@gmail.com>
2022-01-29 02:01:17 +0100

    Add check on number of zeros

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -241,7 +241,8 @@ def main():
 				n= data_1d.size
 				n_zeros= np.count_nonzero(data_1d==0)
 				f= n_zeros/n
-				logger.info("Image %d chan %d (name=%s, label=%s): n=%d, n_zeros=%d, f=%f" % (img_counter, i+1, sname, label, n, n_zeros, f))
+				if n_zeros>0:
+					logger.info("Image %d chan %d (name=%s, label=%s): n=%d, n_zeros=%d, f=%f" % (img_counter, i+1, sname, label, n, n_zeros, f))
 				
 				if f>=fthr_zeros:
 					logger.warn("Image %d chan %d (name=%s, label=%s) has a zero fraction %f, check!" % (img_counter, i+1, sname, label, f))
