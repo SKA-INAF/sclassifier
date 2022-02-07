27075369cebea82af22a12185d8e59b6161c788a
riggi <simone.riggi@gmail.com>
2022-02-07 12:40:01 +0100

    Add options

--- scripts/run_predict.py
+++ scripts/run_predict.py
@@ -63,6 +63,33 @@ def get_args():
 	parser.add_argument('-nx', '--nx', dest='nx', required=False, type=int, default=64, action='store',help='Image resize width in pixels (default=64)')
 	parser.add_argument('-ny', '--ny', dest='ny', required=False, type=int, default=64, action='store',help='Image resize height in pixels (default=64)')	
 
+	parser.add_argument('--normalize', dest='normalize', action='store_true',help='Normalize input images in range [0,1]')	
+	parser.set_defaults(normalize=False)
+	parser.add_argument('--scale_to_abs_max', dest='scale_to_abs_max', action='store_true',help='In normalization, if scale_to_max is active, scale to global max across all channels')	
+	parser.set_defaults(scale_to_abs_max=False)
+	parser.add_argument('--scale_to_max', dest='scale_to_max', action='store_true',help='In normalization, scale to max not to min-max range')	
+	parser.set_defaults(scale_to_max=False)
+
+	parser.add_argument('--log_transform', dest='log_transform', action='store_true',help='Apply log transform to images')	
+	parser.set_defaults(log_transform=False)
+	
+	parser.add_argument('--scale', dest='scale', action='store_true',help='Apply scale factors to images')	
+	parser.set_defaults(scale=False)
+	parser.add_argument('-scale_factors', '--scale_factors', dest='scale_factors', required=False, type=str, default='', action='store',help='Image scale factors separated by commas (default=empty)')
+
+	parser.add_argument('--standardize', dest='standardize', action='store_true',help='Apply standardization to images')	
+	parser.set_defaults(standardize=False)
+	parser.add_argument('-img_means', '--img_means', dest='img_means', required=False, type=str, default='', action='store',help='Image means (separated by commas) to be used in standardization (default=empty)')
+	parser.add_argument('-img_sigmas', '--img_sigmas', dest='img_sigmas', required=False, type=str, default='', action='store',help='Image sigmas (separated by commas) to be used in standardization (default=empty)')
+
+	parser.add_argument('--chan_divide', dest='chan_divide', action='store_true',help='Apply channel division to images')	
+	parser.set_defaults(chan_divide=False)
+	parser.add_argument('-chan_mins', '--chan_mins', dest='chan_mins', required=False, type=str, default='', action='store',help='Image channel means (separated by commas) to be used in chan divide (default=empty)')
+
+	parser.add_argument('--erode', dest='erode', action='store_true',help='Apply erosion to image sourve mask')	
+	parser.set_defaults(erode=False)	
+	parser.add_argument('-erode_kernel', '--erode_kernel', dest='erode_kernel', required=False, type=int, default=5, action='store',help='Erosion kernel size in pixels (default=5)')	
+
 	# - Autoencoder model options
 	parser.add_argument('-modelfile_encoder', '--modelfile_encoder', dest='modelfile_encoder', required=True, type=str, action='store',help='Encoder model architecture filename (.json)')
 	parser.add_argument('-weightfile_encoder', '--weightfile_encoder', dest='weightfile_encoder', required=True, type=str, action='store',help='Encoder model weights filename (.h5)')
@@ -112,6 +139,29 @@ def main():
 	# - Data process options	
 	nx= args.nx
 	ny= args.ny
+
+	normalize= args.normalize
+	scale_to_abs_max= args.scale_to_abs_max
+	scale_to_max= args.scale_to_max
+	log_transform= args.log_transform
+	scale= args.scale
+	scale_factors= []
+	if args.scale_factors!="":
+		scale_factors= [float(x.strip()) for x in args.scale_factors.split(',')]
+	standardize= args.standardize
+	img_means= []
+	img_sigmas= []
+	if args.img_means!="":
+		img_means= [float(x.strip()) for x in args.img_means.split(',')]
+	if args.img_sigmas!="":
+		img_sigmas= [float(x.strip()) for x in args.img_sigmas.split(',')]
+
+	chan_divide= args.chan_divide
+	chan_mins= []
+	if args.chan_mins!="":
+		chan_mins= [float(x.strip()) for x in args.chan_mins.split(',')]
+	erode= args.erode	
+	erode_kernel= args.erode_kernel
 	
 	# - Autoencoder options
 	modelfile_encoder= args.modelfile_encoder
@@ -147,11 +197,24 @@ def main():
 	logger.info("Running autoencoder classifier predict ...")
 	vae_class= FeatExtractorAE(dl)
 	vae_class.set_image_size(nx, ny)
+	vae_class.normalize= normalize
+	vae_class.scale_to_abs_max= scale_to_abs_max
+	vae_class.scale_to_max= scale_to_max
+	vae_class.log_transform_img= log_transform
+	vae_class.scale_img= scale
+	vae_class.scale_img_factors= scale_factors
+	vae_class.standardize_img= standardize
+	vae_class.img_means= img_means
+	vae_class.img_sigmas= img_sigmas
+	vae_class.chan_divide= chan_divide
+	vae_class.chan_mins= chan_mins
+	vae_class.erode= erode
+	vae_class.erode_kernel= erode_kernel
+	vae_class.add_channorm_layer= add_channorm_layer
 	
 	if vae_class.predict_model(modelfile_encoder, weightfile_encoder)<0:
 		logger.error("VAE predict failed!")
 		return 1
-
 
 	#===========================
 	#==   RUN UMAP PREDICT
