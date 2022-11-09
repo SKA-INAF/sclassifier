053ec49fd5b657592eaed767c39f807f9e1d8115
riggi <simone.riggi@gmail.com>
2022-11-09 17:06:22 +0100

    Add ChanPosDef layer

--- sclassifier/tf_utils.py
+++ sclassifier/tf_utils.py
@@ -234,8 +234,8 @@ class ChanMaxScale(layers.Layer):
 		#######  DEBUG ###########
 		data_min= tf.reduce_min(inputs_scaled, axis=(1,2))
 		data_max= tf.reduce_max(inputs_scaled, axis=(1,2))
-		data_min= tf.expand_dims(tf.expand_dims(data_min, axis=1), axis=1)
-		data_max= tf.expand_dims(tf.expand_dims(data_max, axis=1), axis=1)
+		#data_min= tf.expand_dims(tf.expand_dims(data_min, axis=1), axis=1)
+		#data_max= tf.expand_dims(tf.expand_dims(data_max, axis=1), axis=1)
 		
 		tf.print("data_min (after max scale)", data_min, output_stream=sys.stdout)
 		tf.print("data_max (after max scale)", data_max, output_stream=sys.stdout)
@@ -294,8 +294,12 @@ class ChanPosDef(layers.Layer):
 		inputs_scaled= tf.where(~cond, tf.ones_like(inputs_scaled) * norm_min, inputs_scaled)
 		
 		#######  DEBUG ###########
-		data_min= tf.reduce_min(tf.where(~cond, tf.ones_like(inputs_scaled) * 1.e+99, inputs_scaled), axis=(1,2))
-		data_max= tf.reduce_max(tf.where(~cond, tf.ones_like(inputs_scaled) * -1.e+99, inputs_scaled), axis=(1,2))
+		data_min= tf.reduce_min(inputs_scaled, axis=(1,2))
+		data_max= tf.reduce_max(inputs_scaled, axis=(1,2))
+		data_min_nozeros= tf.reduce_min(tf.where(~cond, tf.ones_like(inputs_scaled) * 1.e+99, inputs_scaled), axis=(1,2))
+		data_max_nozeros= tf.reduce_max(tf.where(~cond, tf.ones_like(inputs_scaled) * -1.e+99, inputs_scaled), axis=(1,2))
+		tf.print("data_min (nozeros, after posdef)", data_min_nozeros, output_stream=sys.stdout)
+		tf.print("data_max (nozeros, after posdef)", data_max_nozeros, output_stream=sys.stdout)
 		tf.print("data_min (after posdef)", data_min, output_stream=sys.stdout)
 		tf.print("data_max (after posdef)", data_max, output_stream=sys.stdout)
 		###########################
