3b82d4fb02680d8f3718ad890fce814933193cc8
riggi <simone.riggi@gmail.com>
2022-11-09 15:53:15 +0100

    Add ChanPosDef layer

--- sclassifier/tf_utils.py
+++ sclassifier/tf_utils.py
@@ -288,8 +288,8 @@ class ChanPosDef(layers.Layer):
 		#########################		
 
 		# - Subtract data_min on channels with negative data_min
-		cond2= tf.math.greater(data_min, 0)
-		inputs_scaled= tf.where(~cond2, inputs - data_min, inputs)
+		cond2= tf.math.less(data_min, 0)
+		inputs_scaled= tf.where(cond2, inputs - data_min, inputs)
 
 		# - Set masked values (NANs, zeros) to norm_min
 		norm_min= 0
