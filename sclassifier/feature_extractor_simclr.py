eaa5347cc92bd6d1d8f91ccf4e6b77b1e4d050fc
riggi <simone.riggi@gmail.com>
2022-11-22 12:05:06 +0100

    Add SimCLR feat extractor

--- sclassifier/feature_extractor_simclr.py
+++ sclassifier/feature_extractor_simclr.py
@@ -507,8 +507,8 @@ class FeatExtractorSimCLR(object):
 
 		o = soft_cos_sim(g[-1])  # Output = Last layer of projection head
 
-		logger.info("isinstance(i, list)? " % (isinstance(i, list)))
-		logger.info("isinstance(o, list)? " % (isinstance(o, list)))
+		logger.info("isinstance(i, list)? %d" % (isinstance(i, list)))
+		logger.info("isinstance(o, list)? %d" % (isinstance(o, list)))
      
 		#===========================
 		#==   COMPILE MODEL
