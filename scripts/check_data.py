3537d6a4bc1938a5f6458b02a7c468accfd6c826
riggi <simone.riggi@gmail.com>
2022-12-07 20:23:55 +0100

    Removed translation from CNN augmenter

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -287,11 +287,11 @@ def main():
 	if erode:
 		preprocess_stages.append(MaskShrinker(kernel=erode_kernel))
 	
-	if mask_borders:
-		preprocess_stages.append(BorderMasker(mask_border_fract))
-	
 	if augment:
 		preprocess_stages.append(Augmenter(augmenter_choice=augmenter))
+
+	if mask_borders:
+		preprocess_stages.append(BorderMasker(mask_border_fract))
 
 	if resize:
 		#preprocess_stages.append(Resizer(nx=nx, ny=ny))
