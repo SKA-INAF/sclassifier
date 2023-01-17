c4ebb6dec95c664955d2a45a2502acbfcbd0a3ce
riggi <simone.riggi@gmail.com>
2023-01-17 19:27:46 +0100

    Add batch class balance

--- sclassifier/feature_extractor_ae.py
+++ sclassifier/feature_extractor_ae.py
@@ -622,8 +622,8 @@ class FeatExtractorAE(object):
 
 		# - Retrieve info from data loader
 		self.nchannels= self.dg.nchannels
-		if self.chan_divide:
-			self.nchannels-= 1
+		#if self.chan_divide:
+		#	self.nchannels-= 1
 		self.source_labels= self.dg.labels
 		self.source_ids= self.dg.classids
 		self.source_names= self.dg.snames
