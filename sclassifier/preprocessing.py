70e9a67bc7793a49309eccbd15e28075f85d4e1d
Simone Riggi <simone.riggi@gmail.com>
2023-05-05 12:30:11 +0200

    Modified zscale contrast min

--- sclassifier/preprocessing.py
+++ sclassifier/preprocessing.py
@@ -1788,10 +1788,10 @@ class Augmenter(object):
 		augmenter_simclr7= iaa.Sequential(
 			[
 				iaa.Sometimes(0.1, blur_aug),
+				zscaleStretch2_aug,
 				iaa.OneOf([iaa.Fliplr(1.0), iaa.Flipud(1.0), iaa.Noop()]),
   			iaa.OneOf([iaa.Rot90((1,3)), iaa.Noop()]), # rotate by 90, 180 or 270 or do nothing
-  			iaa.Sometimes(0.5, percThr_aug),
-  			zscaleStretch2_aug
+  			iaa.Sometimes(0.5, percThr_aug) 			
 			]
 		)
 
