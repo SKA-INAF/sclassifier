77018c3054e5594c421f37522b41d3f66ae82d2a
Simone Riggi <simone.riggi@gmail.com>
2024-09-22 11:29:27 +0200

    Update

--- macros/extract_siglip_representation.py
+++ macros/extract_siglip_representation.py
@@ -71,6 +71,8 @@ def get_args():
 	parser.add_argument('--reset_rescale', dest='reset_rescale', action='store_true',help='Reset original rescale transform used in processor (default=false)')	
 	parser.set_defaults(reset_rescale=False)
 	
+	parser.add_argument('--clip_data', dest='clip_data', action='store_true',help='Apply sigma clipping transform (default=false)')	
+	parser.set_defaults(clip_data=False)
 	parser.add_argument('--zscale', dest='zscale', action='store_true',help='Apply zscale transform (default=false)')	
 	parser.set_defaults(zscale=False)
 	parser.add_argument('--norm_min', default=0., type=float, help='Norm min (default=0)')
@@ -80,9 +82,7 @@ def get_args():
 	parser.add_argument('--in_chans', default = 1, type = int, help = 'Length of subset of dataset to use.')
 	parser.add_argument('--set_zero_to_min', dest='shift_zero_to_min', action='store_true',help='Set blank pixels to min>0 (default=false)')	
 	parser.set_defaults(set_zero_to_min=False)
-	parser.add_argument('--center_crop', dest='center_crop', action='store_true', help='Center crop image to fixed desired size in pixel, specified in crop_size option (default=no)')	
-	parser.set_defaults(center_crop=False)
-	parser.add_argument('-crop_size', '--crop_size', dest='crop_size', required=False, type=int, default=224, action='store',help='Crop size in pixels (default=224)')
+	parser.add_argument('--in_chans', default = 1, type = int, help = 'Length of subset of dataset to use.')
 	
 	# - Model option
 	parser.add_argument('-model','--model', dest='model', required=False, type=str, default="google/siglip-so400m-patch14-384", help='SigLIP pretrained model {google/siglip-so400m-patch14-384,google/siglip-so400m-patch14-224,google/siglip-base-patch16-256,google/siglip-large-patch16-256}') 
@@ -98,31 +98,13 @@ def get_args():
 	return args
 	
 	
-def sigma_clipping(data, sigma_low, sigma_up):
-	""" Clip input data """
+def get_clipped_data(self, data, sigma_low=5, sigma_up=30):
+	""" Apply sigma clipping to input data and return transformed data """
 
-	# - Get 1D data
+	# - Find NaNs pixels
 	cond= np.logical_and(data!=0, np.isfinite(data))
 	data_1d= data[cond]
-	
-	# - Subtract background
-	#bkgval, _, _ = sigma_clipped_stats(data_1d, sigma=sigma_bkg)
-	#data_bkgsub= data - bkgval
-	#data= data_bkgsub
 
-	# - Clip all pixels that are below sigma clip
-	#cond= np.logical_and(data!=0, np.isfinite(data))
-	#data_1d= data[cond]
-	#res= sigma_clip(data_1d, sigma_lower=sigma_low, sigma_upper=sigma_up, masked=True, return_bounds=True)
-	#thr_low= float(res[1])
-	#thr_up= float(res[2])
-	##print("thr_low=%f, thr_up=%f" % (thr_low, thr_up))
-
-	#data_clipped= np.copy(data)
-	#data_clipped[data_clipped<thr_low]= thr_low
-	#data_clipped[data_clipped>thr_up]= thr_up
-	
-	
 	# - Clip all pixels that are below sigma clip
 	res= sigma_clip(data_1d, sigma_lower=sigma_low, sigma_upper=sigma_up, masked=True, return_bounds=True)
 	thr_low= res[1]
@@ -134,27 +116,33 @@ def sigma_clipping(data, sigma_low, sigma_up):
 
 	# - Set NaNs to 0
 	data_clipped[~cond]= 0
-	
+
 	return data_clipped
-	
-	
 
 
-def zscale_stretch(data, contrast):
-	""" Apply zscale stretch """	
 	
-	transform = ZScaleInterval(contrast=contrast)
-	data_stretched = transform(data)
-	
-	return data_stretched
+def get_zscaled_data(self, data, contrast=0.25):
+	""" Apply sigma clipping to input data and return transformed data """
 
-def transform_img(data, norm_range, apply_zscale, contrast, clip_data, sigma_low, sigma_up, to_uint8, set_zero_to_min=False):
+	# - Find NaNs pixels
+	cond= np.logical_and(data!=0, np.isfinite(data))
+
+	# - Apply zscale transform
+	transform= ZScaleInterval(contrast=contrast)
+	data_transf= transform(data)	
+
+	# - Set NaNs to 0
+	data_transf[~cond]= 0
+
+	return data_transf
+	
+def transform_img(data, args):
 	""" Transform input data """
-
-	data_transf= np.copy(data)
 	
-	# - Replace nan values with min
-	if set_zero_to_min:
+	data_transf= np.copy(data)
+
+	# - Set NANs to image min
+	if args.set_zero_to_min:
 		cond= np.logical_and(data_transf!=0, np.isfinite(data_transf))
 	else:
 		cond= np.isfinite(data_transf)
@@ -166,26 +154,30 @@ def transform_img(data, norm_range, apply_zscale, contrast, clip_data, sigma_low
 			
 	data_min= np.min(data_1d)
 	data_transf[~cond]= data_min
-	
+
 	print("== DATA MIN/MAX ==")
 	print(data_transf.min())
 	print(data_transf.max())
 
 	# - Clip data?
-	if clip_data:
-		data_clipped= sigma_clipping(data_transf, sigma_low, sigma_up, sigma_bkg)
+	if args.clip_data:
+		data_clipped= get_clipped_data(data_transf, sigma_low=5, sigma_up=30)
 		data_transf= data_clipped
 
 	# - Apply zscale stretch
-	if apply_zscale:
-		data_stretched= zscale_stretch(data_transf, contrast=contrast)
-		data_transf= data_stretched 
+	if args.zscale:
+		print("Apply zscale stretch ...")
+		data_stretched= get_zscaled_data(data_transf, contrast=0.25)
+		data_transf= data_stretched
+
+	# - Convert to uint8
+	#data_transf= (data_transf*255.).astype(np.uint8)
 		
 	# - Normalize to range
 	data_min= data_transf.min()
 	data_max= data_transf.max()
-	norm_min= norm_range[0]
-	norm_max= norm_range[1]
+	norm_min= args.norm_min
+	norm_max= args.norm_max
 	if norm_min==data_min and norm_max==data_max:
 		print("INFO: Data already normalized in range (%f,%f)" % (norm_min, norm_max))
 	else:
@@ -197,11 +189,12 @@ def transform_img(data, norm_range, apply_zscale, contrast, clip_data, sigma_low
 	print(data_transf.max())
 	
 	# - Convert to uint8
-	if to_uint8:
-		data_transf= data_transf.astype(np.uint8)	
-		#data_transf= (data_transf*255.).astype(np.uint8)
-
+	if args.to_uint8:
+		data_transf= data_transf.astype(np.uint8)
+	
 	return data_transf
+
+
 	
 def read_img(filename, args):
 	""" Read fits image """
@@ -211,30 +204,30 @@ def read_img(filename, args):
 		return None
 		
 	# - Read fits image
-	data= fits.open(filename)[0].data
+	if file_ext=='.fits':
+		data= fits.open(filename)[0].data
+	else:
+		image= Image.open(filename)
+		data= np.asarray(image)
+	
 	if data is None:
 		return None
 				
-	# - Apply stretch transform
-	data_transf= transform_img(
-		data, 
-		norm_range=(args.norm_min, args.norm_max),
-		apply_zscale=args.zscale, contrast=0.25, 
-		clip_data=False, sigma_low=5, sigma_up=30,
-		to_uint8=args.to_uint8,
-		set_zero_to_min=args.set_zero_to_min
-	)
+	# - Apply transform to numpy array
+	data_transf= transform_img(data, args)
 	if data_transf is None:
 		return None
 	
-	# - Convert to PIL image RGB?
+	data= data_transf
+	
+	# - Convert numpy to PIL image
+	image = Image.fromarray(data)
+	
+	# - Convert to RGB image
 	if args.in_chans==3:
-		img= Image.fromarray(data_transf).convert("RGB")
-	else:
-		img= Image.fromarray(data_transf)
-	
-	return img
-	
+		image= image.convert("RGB")
+		
+	return image
 	
 	
 	
