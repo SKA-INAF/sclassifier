bfd3d4ab5f1094d92f7d65ed705cf38d793a9f99
Simone Riggi <simone.riggi@gmail.com>
2024-12-16 19:30:42 +0100

    Refactor code, add options

--- sclassifier/clustering.py
+++ sclassifier/clustering.py
@@ -351,7 +351,7 @@ class Clusterer(object):
 				label_list.append(label)
 				self.source_names_preclassified.append(source_name)
 			else:
-				logger.info("Exclude source with id=%d from list (excluded_ids=%s) ..." % (obj_id, str(self.excluded_objids_train)))
+				logger.info("Exclude source with id=%s from list (excluded_ids=%s) ..." % (str(obj_id), str(self.excluded_objids_train)))
 					
 
 		if row_list:	
