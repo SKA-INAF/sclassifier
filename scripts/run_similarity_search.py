2dc6df648875aa317733517185ab9fe372557200
Simone Riggi <simone.riggi@gmail.com>
2024-12-18 13:21:00 +0100

    Add support for computing similarity between input image and dataset

--- scripts/run_similarity_search.py
+++ scripts/run_similarity_search.py
@@ -33,7 +33,8 @@ from sclassifier import __version__, __date__
 from sclassifier import logger
 from sclassifier.utils import Utils
 from sclassifier.utils import NoIndent, MyEncoder
-from sclassifier.faiss_utils import get_top_k_similar_within_data
+from sclassifier.faiss_utils import get_top_k_similar_within_data, get_top_k_similar
+from sclassifier.tf_utils import extract_tf_features_from_img
 
 import matplotlib.pyplot as plt
 
@@ -54,10 +55,20 @@ def get_args():
 	parser = argparse.ArgumentParser(description="Parse args.")
 
 	# - Input options
-	parser.add_argument('-datafile','--datafile', dest='datafile', required=True, type=str, help='Path to feature data file (.json)') 
+	parser.add_argument('-datafile','--datafile', dest='datafile', required=True, type=str, help='Path to feature data file (.json)')
 	parser.add_argument('-datalist_key','--datalist_key', dest='datalist_key', required=False, type=str, default="data", help='Dictionary key name to be read in input datalist (default=data)') 
 	parser.add_argument('-selcols','--selcols', dest='selcols', required=False, type=str, default='', help='Data column ids to be selected from input data, separated by commas') 
 
+	parser.add_argument('-img','--img', dest='img', required=False, default="", type=str, help='Path to image file. If provided the program will find the images in the datafile that are most similar to this image (.fits/.png/.jpg)') 
+	
+	# - Image representation & processing options (to extract image representation)
+	parser.add_argument('-model','--model', dest='model', required=True, type=str, help='Path to model architecture (.h5)') 
+	parser.add_argument('-model_weights','--model_weights', dest='model_weights', required=True, type=str, help='Path to model weights (.h5)') 
+	parser.add_argument('--imgsize', default=224, type=int, help='Image resize size in pixels (default=224)')
+	parser.add_argument('--zscale', dest='zscale', action='store_true',help='Apply zscale transform (default=false)')	
+	parser.set_defaults(zscale=False)
+	parser.add_argument('--zscale_contrast', default=0.25, type=float, help='ZScale transform contrast (default=0.25)')
+	
 	# - Similarity search options
 	parser.add_argument('-k', '--k', dest='k', required=False, type=int, default=10, action='store',help='Number of neighbors in similarity search (default=10)')
 	parser.add_argument('-score_thr', '--score_thr', dest='score_thr', required=False, type=float, default=0.6, action='store',help='Similarity threshold below which neighbors are not include in graph (default=0.0)')
@@ -94,6 +105,11 @@ def main():
 	if args.datafile=="":
 		logger.error("Empty input data filename!")
 		return 1
+		
+	# - Check if image is given
+	run_simsearch_with_img= False
+	if args.img!="":
+		run_simsearch_with_img= True
 		
 	selcols= []
 	if args.selcols!="":
@@ -152,21 +168,50 @@ def main():
 	
 	logger.info("%d data read ..." % (N))
 
+	#=================================
+	#==   EXTRACT FEATURES FROM IMG?
+	#=================================
+	if run_simsearch_with_img:
+		logger.info("Extracting features from file %s ..." % (args.img))
+		data_vector= extract_tf_features_from_img(
+			filename=args.img, 
+			modelfile=args.model,
+			weightfile=args.model_weights, 
+			imgsize=args.imgsize, 
+			zscale=args.zscale, 
+			contrast=args.zscale_contrast
+		)
+		if imgdata is None:
+			logger.error("Failed to extract data representation from image %s!" % (args.img))
+			return 1
+
 	#===========================
 	#==   SIMILARITY SEARCH
 	#===========================
 	# - Compute indices & scores of top similar data
-	logger.info("Compute indices & scores of top similar data ...")
-	
-	nn_indices, nn_scores= get_top_k_similar_within_data(
-		data,
-		k= args.k,
-		threshold= args.score_thr,
-		large_data_thr= args.large_data_thr,
-		nlist= args.nlist,
-		M= args.M,
-		nprobe= args.nprobe
-	)
+	if run_simsearch_with_img:
+		logger.info("Compute indices & scores of top similar data between dataset and image ...")
+		nn_indices, nn_scores= get_top_k_similar(
+			data, 
+			data_vector, 
+			k= args.k,
+			threshold= args.score_thr,
+			large_data_thr= args.large_data_thr,
+			nlist= args.nlist,
+			M= args.M,
+			nprobe= args.nprobe
+		)
+	else:
+		logger.info("Compute indices & scores of top similar data  within dataset ...")
+		nn_indices, nn_scores= get_top_k_similar_within_data(
+			data,
+			k= args.k,
+			threshold= args.score_thr,
+			large_data_thr= args.large_data_thr,
+			nlist= args.nlist,
+			M= args.M,
+			nprobe= args.nprobe
+		)
 	
 	#===========================
 	#==   SAVE OUTPUTS
@@ -175,21 +220,37 @@ def main():
 	logger.info("Prepare output data ...")
 	outdata= {args.datalist_key: []}
 	
-	for i in range(N):
-		feats= list(data[i])
-		feats= [float(item) for item in feats]
-		indices= list(nn_indices[i])
-		indices= [int(item) for item in indices]
-		scores= list(nn_scores[i])
-		scores= [float(item) for item in scores]
+	if run_simsearch_with_img:
+		for i in range(len(nn_indices)):
+			index= nn_indices[i]
+			score= nn_scores[i]
+			feats= list(data[index])
+			feats= [float(item) for item in feats]
+			
+			# - Set output data
+			d= datalist[index]
+			d['feats']= NoIndent(feats)
+			d['nn_index']= index
+			d['nn_score']= score
+			outdata[args.datalist_key].append(d)
+			
+	else:
+		for i in range(N):
+			feats= list(data[i])
+			feats= [float(item) for item in feats]
+			indices= list(nn_indices[i])
+			indices= [int(item) for item in indices]
+			scores= list(nn_scores[i])
+			scores= [float(item) for item in scores]
 		
-		# - Set output data
-		d= datalist[i]
-		d['feats']= NoIndent(feats)
-		d['nn_indices']= NoIndent(indices)
-		d['nn_scores']= NoIndent(scores)
-		outdata[args.datalist_key].append(d)
+			# - Set output data
+			d= datalist[i]
+			d['feats']= NoIndent(feats)
+			d['nn_indices']= NoIndent(indices)
+			d['nn_scores']= NoIndent(scores)
+			outdata[args.datalist_key].append(d)
 
+	
 	# - Write selected datalist
 	logger.info("Write output data to file %s ..." % (args.outfile))
 	
